e7256bf987f2595567f7d9d04898f890477c3c85
Dan Day <danday@gmail.com>
2016-12-19 18:57:47 -0500

    add ellipses

--- shapes.js
+++ shapes.js
@@ -5,6 +5,7 @@ let svg = d3.select("body").append("svg").attr("height", "100%").attr("width", "
 svg.selectAll("rect")
   .data(dataArray)
   .enter().append("rect")
+          .attr("fill", "pink")
           .attr("height",function(d,i) {return d*15;})
           .attr("width", "50")
           .attr("x",function(d,i) { return 60*i; })
@@ -12,9 +13,24 @@ svg.selectAll("rect")
 
 let newX = 300;
 
-svg.selectAll("circle")
+svg.selectAll("circle.first")
       .data(dataArray)
       .enter().append("circle")
-          .attr("cx", function(d,i) {newX +=(d*6)+(i*20); return newX})
+          .attr("class", "first")
+          .attr("fill", "pink")
+          .attr("cx", function(d,i) {newX +=(d*3)+(i*20); return newX})
           .attr("cy", "100")
           .attr("r",function(d,i) {return d*3});
+
+newX = 600;
+
+svg.selectAll("ellipse")
+      .data(dataArray)
+      .enter().append("ellipse")
+          .attr("class", "second")
+          .attr("class", "first")
+          .attr("fill", "pink")
+          .attr("cx", function(d,i) {newX +=(d*3)+(i*20); return newX})
+          .attr("cy", "100")
+          .attr("rx",function(d,i) {return d*3})
+          .attr("ry", "30");
