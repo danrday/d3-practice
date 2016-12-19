4ea6917271280ac3393d9fa1f147b9a6b523552f
Dan Day <danday@gmail.com>
2016-12-19 13:37:36 -0500

    d3 rectangles

--- shapes.js
+++ shapes.js
@@ -1,6 +1,6 @@
 let dataArray = [5,11,18];
 
-var svg = d3.select("body").append("svg").attr("height", "100%").attr("width", "100%");
+let svg = d3.select("body").append("svg").attr("height", "100%").attr("width", "100%");
 
 svg.selectAll("rect")
   .data(dataArray)
